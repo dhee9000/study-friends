e405f8ac811d69c6ffae1cdf634b97900bab1c5f
kulkarni.nikhil89@gmail.com <you@example.com>
2019-02-23 17:46:35 -0600

    wow it aligns where i want it to now

--- views/matchingList.js
+++ views/matchingList.js
@@ -4,19 +4,20 @@ import {AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
 export class MatchingList extends Component{
     render(){
         return(
-            <View>
-            <Text style= {{
-                fontWeight: 'bold',
-                color: 'red',
-                justifyContent: 'flex-start',
-                fontSize: 24,
-                }}>
-                Name:
-                <Text style= {{color: 'black'}}>
-                    Nikhil Kulkarni
-                </Text>
-            </Text>
+            <View style={styles.view}>
+                <Text style={styles.text}>This is Nihkil, and I'm bad at programming.</Text>
             </View>
         );
     }
 };
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        alignItems: 'center', 
+    },
+    text: {
+        color: 'red',
+        fontWeight: 'bold',
+    },
+})
