8f8ec136119f4e2bfa2cabe092eaa1cf69b7f7ec
kulkarni.nikhil89@gmail.com <you@example.com>
2019-02-23 19:49:15 -0600

    all information is displayed but time can't align right

--- views/matchingList.js
+++ views/matchingList.js
@@ -1,23 +1,46 @@
 import React, {Component} from 'react';
-import {AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, TextInput, Platform, StyleSheet, Text, View} from 'react-native';
 
 export class MatchingList extends Component{
     render(){
         return(
-            <View style={styles.view}>
-                <Text style={styles.text}>This is Nihkil, and I'm bad at programming.</Text>
+            <View style={styles.viewStuff}>
+                <Text style={styles.textTitle}>Name: 
+                    <Text style={styles.textInfo}> Nikhil Kulkarni</Text>
+                    <Text style={styles.textTitle}>Time: </Text>
+                    <Text style={styles.textInfo}> 2-3 PM</Text>
+                </Text>
+                <Text style={styles.textTitle}>University:
+                <Text style={styles.textInfo}> The University of Texas at Dallas</Text>
+                </Text>
+                <Text style={styles.textTitle}>Major:
+                <Text style={styles.textInfo}> Computer Science</Text>
+                </Text>
+                <Text style={styles.textTitle}>Year:
+                <Text style={styles.textInfo}> Freshman</Text>
+                </Text>
+                <Text style={styles.textTitle}>List of Classes:
+                <Text style={styles.textInfo}> CS 1336, MATH 2414, PHYS 2325, RHET 1302</Text>
+                </Text>
             </View>
         );
     }
 };
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        alignItems: 'center', 
+    viewStuff: {
+        flex: -1,
+        alignItems: 'flex-start', 
     },
-    text: {
+    viewImportant: {
+        flex: -1,
+        alignItems: 'flex-end',
+    },
+    textTitle: {
         color: 'red',
         fontWeight: 'bold',
+    },
+    textInfo: {
+        color: 'black',
     },
 })
