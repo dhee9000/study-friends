567190cc20a0a221972a38b8ea74cca8011f62f4
kulkarni.nikhil89@gmail.com <you@example.com>
2019-02-23 23:59:30 -0600

    correctly formatted matching list

--- views/matchingList.js
+++ views/matchingList.js
@@ -2,26 +2,29 @@ import React, {Component} from 'react';
 import {AppRegistry, TextInput, Platform, StyleSheet, Text, View} from 'react-native';
 
 export class MatchingList extends Component{
+    
     render(){
+        let name, time1, time2, univ, major, loc, year, class1;
+        name = 'Nikhil Kulkarni', time1 = 1, time2= 2, univ = 'UT Dallas', major = 'CS', loc = 'ECSS', year = 'Freshman', class1 = 'CS 1336';
         return(
             <View style={styles.viewStuff}>
                 <Text style={styles.textTitle}>Name: 
-                <Text style={styles.textInfo}> Nikhil Kulkarni                                  </Text>
+                <Text style={styles.textInfo}> {name}                                  </Text>
                 <Text style={styles.textTitle}>Time: </Text>
-                <Text style={styles.textInfo}> 2-3 PM</Text>
+                <Text style={styles.textInfo}> {time1} to {time2}</Text>
                 </Text>
                 <Text style={styles.textTitle}>University:
-                <Text style={styles.textInfo}> UT Dallas                                    </Text>
+                <Text style={styles.textInfo}> {univ}                                    </Text>
                 <Text style={styles.textTitle}>Location:</Text>
-                <Text style={styles.textInfo}> ECSS</Text>
+                <Text style={styles.textInfo}> {loc}</Text>
                 </Text>
                 <Text style={styles.textTitle}>Major:
-                <Text style={styles.textInfo}> Computer Science;</Text>
+                <Text style={styles.textInfo}> {major};</Text>
                 <Text style={styles.textTitle}> Year: </Text>
-                <Text style={styles.textInfo}> Freshman</Text>
+                <Text style={styles.textInfo}> {year}</Text>
                 </Text>
-                <Text style={styles.textTitle}>List of Classes:
-                <Text style={styles.textInfo}> CS 1336, MATH 2414, PHYS 2325, RHET 1302</Text>
+                <Text style={styles.textTitle}>Class:
+                <Text style={styles.textInfo}> {class1}</Text>
                 </Text>
             </View>
         );
