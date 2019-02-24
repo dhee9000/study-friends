cf7f19c8244ca4d8bb05a51ccb48a050d62e275f
= <=>
2019-02-23 20:27:10 -0600

    Merged ALL Changes

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import AppContainer from './navigators/Home'
 
 export default class App extends Component {
+
   render() {
     return <AppContainer />;
   }
+
 }
